36593d9f75773ac99199b89cd0403be1974e2e10
PauliusBulavas <94542478+PauliusBulavas@users.noreply.github.com>
2022-03-17 19:52:23 +0200

    paskaiciuotas plotas

--- SavarankiskasDarbas/Program.cs
+++ SavarankiskasDarbas/Program.cs
@@ -6,8 +6,10 @@ namespace SavarankiskasDarbas
     {
         static void Main(string[] args)
         {
+            //zemes plotas
 
-
+            double spindulyskvadratu = Math.Pow(6731, 2);
+            double zemesPlotas = 4 * 3.14 * spindulyskvadratu;
         }
     }
 }
